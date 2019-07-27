efb65be3f9d5f2992025864b2587d10689504712
Apimun Klansakul <apimun.ksk@Apimuns-MacBook-Pro.local>
2019-07-27 11:55:57 +0700

    first initial

---
+++ js/secretary.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var title = $('secretary').text();
+    switch (title) {
+        case "display_durable_articles_permits":
+            $('.nav-articles').addClass('active');
+            $('.collapse-articles').addClass('show');
+            $('.collapse-articles .permits').addClass('active');
+            break;
+        case "insert_durable_articles_purchase":
+            $('.nav-articles').addClass('active');
+            $('.collapse-articles').addClass('show');
+            $('.collapse-articles .purchase').addClass('active');
+            break
+    }
+})
