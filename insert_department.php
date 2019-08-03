9a4d3c5d1789b333eca8169569821d4af5312f29
sirirat41 <sirirat1203_@hotmail.com>
2019-08-03 12:06:45 +0700

    add

--- insert_department.php
+++ insert_department.php
@@ -193,6 +193,37 @@
   <script src="js/demo/chart-pie-demo.js"></script>
   <script src="js/secretary.js"></script>
 
+  <script >
+    $(document).ready(function() {
+      <?php
+      if (isset($_GET["message"])); {
+        $message = $_GET["message"];
+        echo "$('#modal-message').modal();";
+      }
+      ?>
+    })
+    </script>
+
+  <div class="modal fade" id="modal-message" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <?php echo $_GET["message"];?>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">ตกลง</button>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
 </body>
 
 </html>
