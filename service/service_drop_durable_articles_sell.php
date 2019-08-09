84c869cb1faf485d52c6937451c21577973b25c0
bongkotchaporn <bongkodporn1141@gmail.com>
2019-08-09 10:33:34 +0700

    add

---
+++ service/service_drop_durable_articles_sell.php
@@ -0,0 +1,17 @@
+<?php
+require "connection.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["product_id"])) {
+    $productid = $_POST["product_id"];
+    $sqlUpdate = "UPDATE durable_articles_sell SET status = 0 WHERE id = " . $productid;
+    if (mysqli_query($conn, $sqlUpdate)) {
+        header('Location: ../display_durable_articles_sell.php?message=ลบข้อมูลสำเร็จ');
+    } else {
+        header('Location: ../display_durable_articles_sell.php?message=ลบข้อมูลไม่สำเร็จ');
+    }
+
+    } else {
+        header('Location: ../display_durable_articles_sell.php?message=ลบข้อมูลผิดพลาด');
+    }
+
+?>
