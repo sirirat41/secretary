aca3d21bafd4d2cdd4f2df61071ae1dcfdceec13
bongkotchaporn <bongkodporn1141@gmail.com>
2019-09-05 15:46:36 +0700

    add

--- display_durable_material_repair_history.php
+++ display_durable_material_repair_history.php
@@ -56,7 +56,7 @@ require "service/connection.php";
                     <button class="btn btn-outline-warning" type="button" onclick="window.location.href='rowback_durable_material_repair_history.php';">
                       <i class="fas fa-sync-alt"></i>
                     </button>
-                    <a rel="tooltip" class="btn btn-outline-primary" href="test.php" target="_blank">
+                    <a rel="tooltip" class="btn btn-outline-primary" href="printall_durable_material_repair_history.php" target="_blank">
                       <i class="fas fa-print"></i>
                     </a>
                 </form>
