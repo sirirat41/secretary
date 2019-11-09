6accf8c5966764343d4e39fce8846bbb10c3d7db
apimun klansakul <apimun.ksk@apimuns-MacBook-Pro.local>
2019-11-09 14:25:09 +0700

    update callback status when edit damage material

--- service/service_edit_durable_material_damage.php
+++ service/service_edit_durable_material_damage.php
@@ -7,11 +7,22 @@ if(isset($_GET['id'])) {
     $damageDate = $_POST["damage_date"];
     $flag = $_POST["flag"];
 
+    $sqlSelect = "SELECT * FROM durable_material_damage WHERE id = $id";
+    $resultOld = mysqli_query($conn, $sqlSelect);
+    $dataOld = mysqli_fetch_assoc($resultOld);
+    $oldProductID = $dataOld["product_id"];
+    $updateOld = "UPDATE durable_material SET status = 1 WHERE id = $oldProductID";
+    mysqli_query($conn, $updateOld);
+
     $log = "แก้ไขข้อมูลการชำรุดวัสดุคงทน รหัส " . $id ;
     logServer($conn, $log);
 
+    $updatePurchase = "UPDATE durable_material SET status = 3";
+    $updatePurchase .= " WHERE id = $productId";
+    mysqli_query($conn, $updatePurchase) or die("Cannot update damage: " . mysqli_error($conn));
+
     $updatePurchase = "UPDATE durable_material_damage SET damage_date = '$damageDate',";
-    $updatePurchase .= " flag = '$flag'";
+    $updatePurchase .= " product_id = $productId, flag = '$flag'";
     $updatePurchase .= " WHERE id = $id";
     mysqli_query($conn, $updatePurchase) or die("Cannot update damage: " . mysqli_error($conn));
     header('Location: ../display_durable_material_damage.php?message=แก้ไขข้อมูลสำเร็จ');
