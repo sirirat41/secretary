4ae133d789f3ca8c611f63124db5a180a6ea9796
sirirat41 <sirirat1203_@hotmail.com>
2019-11-09 19:05:01 +0700

    add

--- edit_durable_material_damage.php
+++ edit_durable_material_damage.php
@@ -70,11 +70,31 @@ if (isset($_GET["id"])) {
             <br>
             <div class="card-body">
               <form method="post" action="service/service_edit_durable_material_damage.php?id=<?php echo $id; ?>" id="form_insert">
-                <div class="row">
-                  <div class="col-md-12 ">
+              <div class="row">
+                  <div class="col-12 ">
                     <div class="form-group">
                       <label for="product_id">รหัสวัสดุ</label>
-                      <input class="form-control" name="product_id" type="text" placeholder="product_id" id="product_id" value="<?php echo $item["code"]; ?>" readonly>
+                      <div class="row">
+                        <div class="col-10 ">
+                          <select class="form-control" name="product_id" id="product_id">
+                            <?php
+                            $sqlSelectType = "SELECT * FROM durable_material WHERE status = 1";
+                            $resultType = mysqli_query($conn, $sqlSelectType);
+                            while ($row = mysqli_fetch_assoc($resultType)) {
+                              if ($item["product_id"] == $row["id"]) {
+                              echo '<option value="' . $row["id"] .'"selected>' . $row["code"] . '</option>';
+                            } else {
+                              echo '<option value="' . $row["id"] . '">' . $row["code"] . '</option>';
+                            }
+                            }
+                            ?>
+                          </select>
+                        </div>
+                        <div class="col-md-2">
+                          <button class="btn btn-outline-danger" type="button" data-toggle="modal" data-target="#modal-form-search" onclick="search()">
+                            <i class="fas fa-search"></i>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
--- service/service_search_json_durable_material.php
+++ service/service_search_json_durable_material.php
@@ -3,11 +3,11 @@
     require 'connection.php';
 
     if (isset($_GET["keyword"])){
-        $keyword = $_GET["keyword"];
+        $keyword = isset($_GET["keyword"]) ? $_GET["keyword"] : null;
         $thai = thainumDigit($keyword);
         $arabic = arabicnumDigit($keyword);
         $sqlSelect = "SELECT m.*, t.name FROM durable_material as m, durable_material_type as t";
-        $sqlSelect .=" WHERE m.type = t.id and ";
+        $sqlSelect .=" WHERE m.type = t.id and m.status = 1";
         $sqlSelect .=" and (m.code like '%$thai%' or m.bill_no like '%$thai%' or t.name like '%$thai%'";
         $sqlSelect .= " or m.code like '%$arabic%' or m.bill_no like '%$arabic%' or t.name like '%$arabic%')";
         $data = array();
